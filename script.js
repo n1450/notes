e82867ccdec34ee02b31a22d46a7a508a5123202
nadia <sa20b025@technikum-wien.at>
2022-09-26 12:31:47 +0200

    script aktualisiert

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+const input = document.getElementById("note");
+const addButton = document.getElementById("add");
+const list = document.getElementById("list");
+
+addButton.addEventListener("click", handleButton);
+
+function handleButton(){
+    console.log(input.value);
+    const item = document.createElement("li");
+    item.innerText = input.value;
+
+    list.appendChild(item);
+    input.value = "";
+}
