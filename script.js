e328a62838ffb9bfdfa399ad83d32f6c738d6cda
nadia1450 <alinadianeha@gmail.com>
2022-10-09 11:25:11 +0200

    Step 10: rerfractor app using MVC pattern

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+
+//Model
+const item = [
+ //"note1"
+ //"note2"
+ //"note3"
+]
+//View
+//see HTML
+function buildLIItem(note){
+const item = document.createElement("li");
+item.textContent = note;
+item.addEventListener("click", handleClickLIItem);
+return item;
+}
+
+//Controller
 document.addEventListener("DOMContentLoaded", function(){
 const button = document.getElementById("add");
 button.addEventListener("click", handleClick);
+
 const input = document.getElementById("text");
 input.getEventListener("keydown", handleKeyDown);
 }
@@ -25,10 +43,9 @@ const input = document.getElementById("text");
 
     if(note){
         const list = document.getElementById("list");
-        const item = document.getElementById("li");
-        item.textContent = note;
-        item.addEventListener("click", handleClickLIItem);
+        const item = buildLIItem("li");
         list.appendChild(item);
+        notes.push(note);
         input.value = "";
         input.focus();
 
